a62672736aaca0ad86dd833804864c283a0e40d4
Pulkit <kpulkit15234@gmail.com>
2024-11-12 10:39:44 +0530

    feat: added linitng file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,131 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
-export default {
+const config: Config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
+    keyframes: {
+      bump: {
+        "44%": {
+          transform: "translate(1.33%, 6.75%)",
+        },
+        "53%": {
+          transform: "translate(-16.67%, -0.54%)",
+        },
+        "61%": {
+          transform: "translate(3.66%, -2.46%)",
+        },
+        "69%": {
+          transform: "translate(-0.59%, 15.27%)",
+        },
+        "76%": {
+          transform: "translate(-1.92%, -4.68%)",
+        },
+        "83%": {
+          transform: "translate(9.38%, 0.96%)",
+        },
+        "90%": {
+          transform: "translate(-4.55%, 1.98%)",
+        },
+      },
+      worm: {
+        from: {
+          strokeDashoffset: "10",
+        },
+        "25%": {
+          strokeDashoffset: "295",
+        },
+        to: {
+          strokeDashoffset: "1165",
+        },
+      },
+    },
+    animation: {
+      bump: "bump 3s linear infinite",
+      worm: "worm 3s cubic-bezier(0.42, 0.17, 0.75, 0.83) infinite",
+    },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [tailwindcssAnimate],
+};
+export default config;
