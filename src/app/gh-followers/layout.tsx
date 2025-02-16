6813ad108ecb0c75278781af34228c0abc0d193a
pulkit <kpulkit15234@gmail.com>
2025-02-16 22:26:09 +0530

    feat: open graph banner for gh-followers page

---
+++ src/app/gh-followers/layout.tsx
@@ -0,0 +1,33 @@
+import assets from "@/assets";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://devpulkit.in"),
+  title: "Gtihu Followers - Pulkit",
+  description: "Explore Pulkit's GitHub followers and their contributions to the developer community.",
+  openGraph: {
+    title: "Gtihu Followers - Pulkit",
+    description: "Take a look at Pulkit's github followers and their contributions to the developer community.",
+    url: "https://devpulkit.in/ghFollowers",
+    type: "website",
+    images: [
+      {
+        url: assets.banner.ghFollowers.src,
+        width: 1200,
+        height: 630,
+        alt: "Github Followers Showcase"
+      }
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Gtihu Followers - Pulkit",
+    description: "Take a look at Pulkit's github followers and their contributions to the developer community.",
+    images: [assets.banner.ghFollowers.src]
+  }
+};
+
+export default function GithubFollowersLayout({ children }: { children: React.ReactNode }) {
+  return children;
+}
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,6 +3,7 @@ import CONTACT_BANNER from "@/assets/banner/contact-banner.webp";
 import DESIGNS_BANNER from "@/assets/banner/designs-banner.webp";
 import EVENTS_BANNER from "@/assets/banner/events-banner.webp";
 import EXPERIENCE_BANNER from "@/assets/banner/experience-banner.webp";
+import GithubFollowersBanner from "@/assets/banner/gh-followers-banner.png";
 import HOME_BANNER from "@/assets/banner/home-banner.webp";
 import PRS_BANNER from "@/assets/banner/prsPage-banner.webp";
 import RESUME_BANNER from "@/assets/banner/resumePage-banner.webp";
@@ -98,6 +99,7 @@ const assets = {
   hacktoberFest: HACKTOBERFEST,
   banner: {
     home: HOME_BANNER,
+    ghFollowers: GithubFollowersBanner,
     contact: CONTACT_BANNER,
     blogs: BLOGS_BANNER,
     designs: DESIGNS_BANNER,
