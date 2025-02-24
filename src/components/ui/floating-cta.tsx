d4aaff9f849546665752fdb537300b7e2d04c513
Pulkit <kpulkit15234@gmail.com>
2025-02-24 16:59:41 +0530

    fix: floating-cta design

--- src/components/ui/floating-cta.tsx
+++ src/components/ui/floating-cta.tsx
@@ -34,18 +34,18 @@ function CollabInviteLayerCX() {
             stiffness: 260,
             damping: 20
           }}
-          className="sticky top-0 z-50 w-full overflow-hidden border-b border-[#1a1a1a] bg-[#0a0a0ae3] shadow-lg shadow-[#00ff9580]"
+          className="sticky top-0 z-50 w-full overflow-hidden border-b border-[#1a1a1a] border-b-[#4fa17f80] bg-[#0a0a0ae3] shadow-2xl shadow-[#4fa17f80]"
         >
           <div className="mx-auto flex max-w-screen-xl items-center justify-between px-4 py-2 md:px-6">
             <div className="flex-1" />
             <PreFetchUrl
               href="/contact"
-              className="cursor-pointer text-center text-sm font-medium text-[#00ff95] md:text-base"
+              className="cursor-pointer text-center text-sm font-medium text-green-400 md:text-base"
               onClick={() => {
                 setShow(false);
               }}
             >
-              Would you like to collab with me or hire me?{show ? "true" : "false"}
+              Would you like to collab with me or hire me?
             </PreFetchUrl>
             <div className="flex flex-1 justify-end">
               <motion.button
