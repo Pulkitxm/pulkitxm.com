8e6612a17b01e0906d3ba1f484d94c8be4607ef3
Pulkit <kpulkit15234@gmail.com>
2025-03-14 20:04:50 +0530

    chore: ended my time at crowdvolt :( & gfg

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,8 +9,7 @@ export function formatDate(dateString: string | Date): string {
   const date = new Date(dateString);
   return date.toLocaleDateString("en-US", {
     year: "numeric",
-    month: "short",
-    day: "numeric"
+    month: "short"
   });
 }
 
