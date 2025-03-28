bec5f56f0d9d9441f3c27b3e89f41293410157da
Pulkit <kpulkit15234@gmail.com>
2025-03-28 14:26:41 +0530

    fix: minor changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import { getLatestWorkflow } from "@/actions/gh";
 import { formatTimeUpdatedAgo } from "@/lib/utils";
 
-export default async function Footer() {
-  const res = await getLatestWorkflow();
+export default function Footer() {
+  const [lastUpdated, setLastUpdated] = useState<Date | null>(null);
 
-  let lastUpdated = new Date();
-
-  if (res.status === "success") {
-    lastUpdated = res.data.timeStamp;
-  }
+  useEffect(() => {
+    getLatestWorkflow().then((res) => {
+      if (res.status === "error") {
+        setLastUpdated(null);
+      } else {
+        setLastUpdated(res.data.timeStamp);
+      }
+    });
+  }, []);
 
   return (
     <div className="mt-8 border-t border-gray-500 py-3 text-center opacity-70">
       <p>Made with ❤️ by Pulkit</p>
       <p className={`mt-1 text-sm text-gray-300 ${lastUpdated === null ? "opacity-0" : ""}`}>
-        Last updated {formatTimeUpdatedAgo(lastUpdated)}
+        Last updated {lastUpdated ? formatTimeUpdatedAgo(lastUpdated) : ""}
       </p>
     </div>
   );
 }
-
-export const dynamic = "force-dynamic";
