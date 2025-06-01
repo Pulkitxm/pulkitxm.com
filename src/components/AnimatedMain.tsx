a7c0157955791d012952a0d7611b04978a37e4f6
Pulkit <kpulkit15234@gmail.com>
2025-06-01 14:46:22 +0530

    refactor: standardize package names in pnpm-lock.yaml and enhance AnimatedMain component with framer-motion for smoother transitions

--- src/components/AnimatedMain.tsx
+++ src/components/AnimatedMain.tsx
@@ -1,11 +1,10 @@
 "use client";
 
+import { motion, HTMLMotionProps } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
-export default function AnimatedMain({
-  ...props
-}: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>) {
+export default function AnimatedMain({ ...props }: HTMLMotionProps<"main">) {
   const pathname = usePathname();
   const [key, setKey] = useState("");
 
@@ -13,5 +12,13 @@ export default function AnimatedMain({
     setKey(pathname);
   }, [pathname]);
 
-  return <main key={key} {...props} />;
+  return (
+    <motion.main
+      key={key}
+      initial={{ opacity: 0, filter: "blur(10px)" }}
+      animate={{ opacity: 1, filter: "blur(0px)" }}
+      transition={{ duration: 1, ease: "easeOut" }}
+      {...props}
+    />
+  );
 }
